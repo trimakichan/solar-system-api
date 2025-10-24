3ee31c7187c11f838b50e458a4d2ddf7055d706b
May Lee <mayhclee@gmail.com>
2025-10-24 12:08:54 -0700

    worked on Wave 3 -- incorporated SQLAlchemy

--- app/models/planet.py
+++ app/models/planet.py
@@ -1,14 +1,24 @@
-class Planet():
-    def __init__(self, id, name, description, distance_from_sun):
-        self.id = id
-        self.name = name
-        self.description = description
-        self.distance_from_sun = distance_from_sun
+from sqlalchemy.orm import Mapped, mapped_column
+from ..db import db
 
-# They are exactly the same — 57_910_000 and 57910000 — but the first one is easier to read. 
-planets = [
-    Planet(1, "Mercury", "Smallest planet, closest to the Sun", 57_910_000),
-    Planet(2, "Earth", "Our beautiful home", 149_600_000),
-    Planet(3, "Venus", "Second planet, covered with thick clouds of sulfuric acid", 108_200_000),
-    Planet(4, "Jupiter", "Largest planet with a massive storm", 778_500_000)
-]
+class Planet(db.Model):
+    id:Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str] 
+    description: Mapped[str]
+    distance_from_sun: Mapped[int]
+
+
+# class Planet():
+#     def __init__(self, id, name, description, distance_from_sun):
+#         self.id = id
+#         self.name = name
+#         self.description = description
+#         self.distance_from_sun = distance_from_sun
+
+# # They are exactly the same — 57_910_000 and 57910000 — but the first one is easier to read. 
+# planets = [
+#     Planet(1, "Mercury", "Smallest planet, closest to the Sun", 57_910_000),
+#     Planet(2, "Earth", "Our beautiful home", 149_600_000),
+#     Planet(3, "Venus", "Second planet, covered with thick clouds of sulfuric acid", 108_200_000),
+#     Planet(4, "Jupiter", "Largest planet with a massive storm", 778_500_000)
+# ]
--- app/routes/planet_routes.py
+++ app/routes/planet_routes.py
@@ -1,45 +1,88 @@
-from flask import Blueprint, abort, make_response
-from app.models.planet import planets
+from flask import Blueprint, abort, make_response, request
+from app.models.planet import Planet
+from ..db import db
+# from app.models.planet import planets
 
 planets_bp = Blueprint("planets_bp", __name__, url_prefix="/planets")
 
+@planets_bp.post("/")
+def create_planet():
+    request_body = request.get_json()
+    # try:
+    name = request_body["name"]
+    description = request_body["description"]
+    distance_from_sun = request_body["distance_from_sun"]
+    # except KeyError:
+    #     msg = {"message: ": "Please provide a valid name, description and distance from sun."}
+    #     abort(make_response(msg,401))
+
+    new_planet = Planet(name=name,  description=description, distance_from_sun=distance_from_sun)
+
+    db.session.add(new_planet)
+    db.session.commit()
+
+    response = {
+        "id": new_planet.id,
+        "name": new_planet.name,
+        "description": new_planet.description,
+        "distance_from_sun": new_planet.distance_from_sun
+    }
+    return response, 201
+
 @planets_bp.get("/")
 def get_all_planets():
+    query = db.select(Planet).order_by(Planet.id)
+    planets = db.session.scalars(query)
+
     planets_response = []
-
     for planet in planets:
-        planet_dict = generate_dict(planet)
-        planets_response.append(planet_dict)
-
+        planets_response.append(
+            {
+                "id": planet.id,
+                "name": planet.name,
+                "description": planet.description,
+                "distance_from_sun": planet.distance_from_sun
+            }
+        )
     return planets_response
 
-@planets_bp.get("/<planet_id>")
-def get_one_planet(planet_id):
-    planet = validate_planet(planet_id)
+# @planets_bp.get("/")
+# def get_all_planets():
+#     planets_response = []
 
-    return generate_dict(planet)
+#     for planet in planets:
+#         planet_dict = generate_dict(planet)
+#         planets_response.append(planet_dict)
 
-def validate_planet(planet_id):
-    try:
-        planet_id = int(planet_id)
+#     return planets_response
+
+# @planets_bp.get("/<planet_id>")
+# def get_one_planet(planet_id):
+#     planet = validate_planet(planet_id)
+
+#     return generate_dict(planet)
+
+# def validate_planet(planet_id):
+#     try:
+#         planet_id = int(planet_id)
         
-    except ValueError:
-        invalid = {"message": f"Planet id {planet_id} is invalid."}
+#     except ValueError:
+#         invalid = {"message": f"Planet id {planet_id} is invalid."}
 
-        abort(make_response(invalid, 400))
+#         abort(make_response(invalid, 400))
     
-    for planet in planets:
-        if planet.id == planet_id:
-            return planet
+#     for planet in planets:
+#         if planet.id == planet_id:
+#             return planet
         
-    not_found = {"message": f"Planet with id {planet_id} not found."}
-    abort(make_response(not_found, 404))
+#     not_found = {"message": f"Planet with id {planet_id} not found."}
+#     abort(make_response(not_found, 404))
 
 
-def generate_dict(planet):
-    return {
-        "id": planet.id,
-        "name": planet.name,
-        "description": planet.description,
-        "distance_from_sun": planet.distance_from_sun
-    }
+# def generate_dict(planet):
+#     return {
+#         "id": planet.id,
+#         "name": planet.name,
+#         "description": planet.description,
+#         "distance_from_sun": planet.distance_from_sun
+#     }
