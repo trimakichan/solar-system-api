544c5e6f013afc52b1c7720ea1fd90cd861e89a5
Makiko <trimakichan@gmail.com>
2025-10-28 12:03:12 -0700

    complete wave 4 and also reorder functions.

--- app/routes/planet_routes.py
+++ app/routes/planet_routes.py
@@ -1,9 +1,26 @@
-from flask import Blueprint, abort, make_response, request
+from flask import Blueprint, abort, make_response, request, Response
 from app.models.planet import Planet
 from ..db import db
-# from app.models.planet import planets
 
 planets_bp = Blueprint("planets_bp", __name__, url_prefix="/planets")
+
+@planets_bp.get("/")
+def get_all_planets():
+    query = db.select(Planet).order_by(Planet.id)
+    planets = db.session.scalars(query)
+
+    planets_response = []
+    for planet in planets:
+        planets_response.append(
+            generate_dict(planet)
+        )
+    return planets_response
+
+@planets_bp.get("/<planet_id>")
+def get_one_planet(planet_id):
+    planet = validate_planet(planet_id)
+    return generate_dict(planet)
+
 
 @planets_bp.post("/")
 def create_planet():
@@ -24,68 +41,56 @@ def create_planet():
     db.session.add(new_planet)
     db.session.commit()
 
-    response = {
-        "id": new_planet.id,
-        "name": new_planet.name,
-        "description": new_planet.description,
-        "distance_from_sun": new_planet.distance_from_sun
-    }
+    response = generate_dict(new_planet)
     return response, 201
 
-@planets_bp.get("/")
-def get_all_planets():
-    query = db.select(Planet).order_by(Planet.id)
-    planets = db.session.scalars(query)
 
-    planets_response = []
-    for planet in planets:
-        planets_response.append(
-            {
-                "id": planet.id,
-                "name": planet.name,
-                "description": planet.description,
-                "distance_from_sun": planet.distance_from_sun
-            }
-        )
-    return planets_response
+@planets_bp.put("/<planet_id>")
+def replace_planet(planet_id):
+    planet = validate_planet(planet_id)
+    request_body = request.get_json()
 
-# @planets_bp.get("/")
-# def get_all_planets():
-#     planets_response = []
+    name = request_body["name"]
+    description = request_body["description"]
+    distance_from_sun = request_body["distance_from_sun"]
 
-#     for planet in planets:
-#         planet_dict = generate_dict(planet)
-#         planets_response.append(planet_dict)
+    planet.name = name
+    planet.description = description
+    planet.distance_from_sun = distance_from_sun
+    db.session.commit()
 
-#     return planets_response
+    return Response(status=204, mimetype="application/json")
 
-# @planets_bp.get("/<planet_id>")
-# def get_one_planet(planet_id):
-#     planet = validate_planet(planet_id)
 
-#     return generate_dict(planet)
+@planets_bp.delete("/<planet_id>")
+def delete_planet(planet_id):
+    planet = validate_planet(planet_id)
+    db.session.delete(planet)
+    db.session.commit()
 
-# def validate_planet(planet_id):
-#     try:
-#         planet_id = int(planet_id)
+    return Response(status=204, mimetype="application/json")
+
+def validate_planet(planet_id):
+    try:
+        planet_id = int(planet_id)
+    except ValueError:
+        msg = {"message": f"Planet {planet_id} invalid."}
+        abort(make_response(msg, 400))
+
+    query = db.select(Planet).where(Planet.id == planet_id)
+    planet = db.session.scalar(query)
+
+    if not planet:
+        msg = {"message" : f"Planet {planet_id} is not found."}  
+        abort(make_response(msg, 404))
         
-#     except ValueError:
-#         invalid = {"message": f"Planet id {planet_id} is invalid."}
-
-#         abort(make_response(invalid, 400))
-    
-#     for planet in planets:
-#         if planet.id == planet_id:
-#             return planet
-        
-#     not_found = {"message": f"Planet with id {planet_id} not found."}
-#     abort(make_response(not_found, 404))
+    return planet
 
 
-# def generate_dict(planet):
-#     return {
-#         "id": planet.id,
-#         "name": planet.name,
-#         "description": planet.description,
-#         "distance_from_sun": planet.distance_from_sun
-#     }
+def generate_dict(planet):
+    return {
+        "id": planet.id,
+        "name": planet.name,
+        "description": planet.description,
+        "distance_from_sun": planet.distance_from_sun
+    }
